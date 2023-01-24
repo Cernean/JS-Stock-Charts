3307ff79f03f516c6df050a4ecdbd9aedfd93078
Cernean <ccernean@yahoo.com>
2023-01-23 21:14:42 -0500

    Not Working

--- public/index.js
+++ public/index.js
@@ -1,40 +1,43 @@
-async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
-}
-
-main()
-
-a1bf862708814b28acbf69cade54b84d
-function getColor(stock) {
-    if(stock === "GME") {
+function getColor(stock){
+    if(stock === "GME"){
         return 'rgba(61, 161, 61, 0.7)'
     }
-    if(stock === "MSFT") {
+    if(stock === "MSFT"){
         return 'rgba(209, 4, 25, 0.7)'
     }
-    if(stock === "DIS") {
+    if(stock === "DIS"){
         return 'rgba(18, 4, 209, 0.7)'
     }
-    if (stock === "BNTX") {
-        return ' rgbs(166, 43, 158, 0.7)'
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
     }
 }
+async function main() {
+    const timeChartCanvas = document.querySelector('#time-chart');
 
-stocks.forEach( stock => stock.value.reverse())
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=a1bf862708814b28acbf69cade54b84d`)
 
-new Chart (timeChartCanvas.getContext('2d') , {
-    type: 'line',
-    data: {
-        labels: stock[0]. value.map(value => value.datetime),
-        datasets: stocks.map( stock => ({
-            label: stock.meta.symbol, 
-            data: stock.value.map(value => parseFloat(value.high)),
-            backgroundColor: getColor(stock.meta.symbol),
-            borderColor: getColor(stock.meta.symbol),
-        }))
-    }
-});
+    const result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+    
+    const stocks = [GME, MSFT, DIS, BNTX];
+    
+    stocks.forEach( stock => stock.values.reverse())
+}
+
+    main()
+
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    });
